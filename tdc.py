7cade55537d384c0b6130d74f6c36b5b853273a4
9iovaferra <ferrari.g1308@gmail.com>
2025-01-20 12:42:05 +0100

    Support for .ini file

--- tdc.py
+++ tdc.py
@@ -1,10 +1,11 @@
 """ Copyright (C) 2019 Pico Technology Ltd. """
+from picosdk.ps6000 import ps6000 as ps
+from picosdk.functions import adc2mV, assert_pico_ok
+from pycoviewlib.functions import *
+from pycoviewlib.constants import chInputRanges
 from ctypes import c_int16, c_uint16, c_int32, c_float, Array, byref
 import numpy as np
-from picosdk.ps6000 import ps6000 as ps
-from pycoviewlib.functions import *
 import matplotlib.pyplot as plt
-from picosdk.functions import adc2mV, assert_pico_ok
 from datetime import datetime as dt
 import sys
 from pathlib import Path
@@ -15,41 +16,6 @@ from time import time as get_time
 # Delay error simulation
 from fitter import Fitter, get_common_distributions, get_distributions
 from statistics import fmean
-
-# def detect_gate_open_closed(
-#		buffer: Array[c_int16],
-#		time: np.ndarray[np.float32],
-#		threshold: float,
-#		maxSamples: int,
-#		timeIntervalns: float
-#		) -> dict[str, float | int]:
-#	"""
-#	minValueIndex = array index of the buffer's negative peak
-#		(most negative value).
-#	Threshold hits are returned as (voltage, time) coordinates.
-#	"""
-#	gateChX = {
-#		"open": {"mV": threshold, "ns": 0.0, "index": 0},
-#		"closed": {"mV": threshold, "ns": 0.0, "index": 0}
-#		}
-#	hit = 0
-#	minValueIndex = buffer.index(min(buffer))
-#	minDifference = min(buffer) - threshold
-#	for i in range(minValueIndex):
-#		if abs(buffer[i] - threshold) < abs(minDifference):
-#			hit = i
-#			minDifference = buffer[i] - threshold
-#	gateChX["open"]["ns"] = time[hit]
-#	gateChX["open"]["index"] = hit
-#	minDifference = min(buffer) - threshold
-#	for i in range(minValueIndex, maxSamples):
-#		if abs(buffer[i] - threshold) < abs(minDifference):
-#			hit = i
-#			minDifference = buffer[i] - threshold
-#	gateChX["closed"]["ns"] = time[hit]
-#	gateChX["closed"]["index"] = hit
-#	
-#	return gateChX
 
 def plot_data(
 		bufferChAmV: Array[c_int16],
@@ -211,40 +177,27 @@ def main():
 	datahandle = f"./Data/tdc_data_{timestamp}.{runtimeOptions['dformat']}"
 	with open(datahandle, "a") as out:
 		out.write("cap\t\tdeltaT (ns)\n")
+	
+	""" Reading runtime parameters from .ini file """
+	params = {}
+	with open("config.ini", "r") as ini:
+		for p in ini.readlines()[1:]:
+			p = p.rstrip("\n").split(" = ")
+			params[p[0]] = int(p[1]) if p[1].isdigit() else float(p[1])
+	params["maxSamples"] = params["preTrigSamples"] + params["postTrigSamples"]
+	
+	""" Logging runtime parameters """
+	if runtimeOptions["log"]:
+		log(loghandle, "==> Running acquisition with parameters:", time=True)
+		colWidth = max([len(k) for k in params.keys()])
+		for key, value in params.items():
+			log(loghandle, f"{key: <{colWidth}} {value:}")
 
 	""" Creating chandle and status. Setting acquisition parameters.
 	Opening ps6000 connection: returns handle for future use in API functions
 	"""
 	chandle = c_int16()
 	status = {}
-
-	chInputRanges = [
-			10, 20, 50, 100, 200, 500, 1000, 2000,
-			5000, 10000, 20000, 50000, 100000, 200000
-			]
-	delaySeconds = 0
-	chRange = 5
-	analogOffset = 0.45
-	thresholdADC = 10000
-	autoTrigms = 10000
-	preTrigSamples = 100
-	postTrigSamples = 250
-	maxSamples = preTrigSamples + postTrigSamples
-	timebase = 1
-	terminalResist = 50
-
-	""" Logging runtime parameters """
-	if runtimeOptions["log"]:
-		log(loghandle, "==> Running acquisition with parameters:", time=True)
-		params = dict(zip(
-			["delaySeconds", "chRangemV", "analogOffset", "thresholdADC",
-			"autoTrigms", "preTrigSamples", "postTrigSamples", "maxSamples",
-			"timebase", "terminalResist"],
-			[0, 500, 0.45, 10000, 10000, 50, 250, maxSamples, 1, 5, 0]
-			))
-		colWidth = max([len(k) for k in params.keys()])
-		for key, value in params.items():
-			log(loghandle, f"{key: <{colWidth}} {value:}")
 
 	status["openUnit"] = ps.ps6000OpenUnit(byref(chandle), None)
 	assert_pico_ok(status["openUnit"])
@@ -259,13 +212,21 @@ def main():
 	offset		0V			0V
 	bandwidth	Full=0		Full=0
 	"""
-	status["setChA"] = ps.ps6000SetChannel(chandle, 0, 1, 1, chRange, analogOffset, 0)
+	status["setChA"] = ps.ps6000SetChannel(
+			chandle, 0, 1, 1, params["chRange"], params["analogOffset"], 0
+			)
 	assert_pico_ok(status["setChA"])
-	status["setChB"] = ps.ps6000SetChannel(chandle, 1, 0, 1, chRange, 0, 0)
+	status["setChB"] = ps.ps6000SetChannel(
+			chandle, 1, 0, 1, params["chRange"], 0, 0
+			)
 	assert_pico_ok(status["setChB"])
-	status["setChC"] = ps.ps6000SetChannel(chandle, 2, 1, 1, chRange, analogOffset, 0)
+	status["setChC"] = ps.ps6000SetChannel(
+			chandle, 2, 1, 1, params["chRange"], params["analogOffset"], 0
+			)
 	assert_pico_ok(status["setChC"])
-	status["setChD"] = ps.ps6000SetChannel(chandle, 3, 0, 1, chRange, 0, 0)
+	status["setChD"] = ps.ps6000SetChannel(
+			chandle, 3, 0, 1, params["chRange"], 0, 0
+			)
 	assert_pico_ok(status["setChD"])
 	
 	""" Setting up trigger on channel A and C
@@ -274,10 +235,7 @@ def main():
 	nTrigConditions = 2
 	channel directions = FALLING (both)
 	threshold = 15000 ADC counts ~=-300mV (both)
-	hysteresis = 1.5% of thresholdADC
-		(distance by which signal must fall above/below the
-		lower/upper threshold in order to re-arm the trigger)
-	auto Trigger = 10000ms (both)
+	auto Trigger = 5000ms (both)
 	delay = 0s (both)
 	"""
 	triggerConditions = ps.PS6000_TRIGGER_CONDITIONS(
@@ -296,33 +254,24 @@ def main():
 			)
 	assert_pico_ok(status["setTriggerChannelConditions"])
 
-	# hysteresis = c_uint16(int(thresholdADC * 0.015))
 	Properties = ps.PS6000_TRIGGER_CHANNEL_PROPERTIES * 2
 	channelProperties = Properties(
 			ps.PS6000_TRIGGER_CHANNEL_PROPERTIES(
-				thresholdADC,
-				0,	# hysteresis,
-				0,	# (thresholdADC * -1),
-				0,	# hysteresis,
-				ps.PS6000_CHANNEL["PS6000_CHANNEL_A"],
+				params["thresholdADC"], 0, 0, 0, ps.PS6000_CHANNEL["PS6000_CHANNEL_A"],
 				ps.PS6000_THRESHOLD_MODE["PS6000_LEVEL"]
 				),
 			ps.PS6000_TRIGGER_CHANNEL_PROPERTIES(
-				thresholdADC,
-				0,	# hysteresis,
-				0,	# (thresholdADC * -1),
-				0,	# hysteresis,
-				ps.PS6000_CHANNEL["PS6000_CHANNEL_C"],
+				params["thresholdADC"], 0, 0, 0, ps.PS6000_CHANNEL["PS6000_CHANNEL_C"],
 				ps.PS6000_THRESHOLD_MODE["PS6000_LEVEL"]
 				)
 			)
 	nChannelProperties = 2
 	status["setTriggerChProperties"] = ps.ps6000SetTriggerChannelProperties(
-			chandle, byref(channelProperties), nChannelProperties, 0, autoTrigms
+			chandle, byref(channelProperties), nChannelProperties, 0, params["autoTrigms"]
 			)
 	assert_pico_ok(status["setTriggerChProperties"])
 
-	status["setTriggerDelay"] = ps.ps6000SetTriggerDelay(chandle, delaySeconds)
+	status["setTriggerDelay"] = ps.ps6000SetTriggerDelay(chandle, params["delaySeconds"])
 	assert_pico_ok(status["setTriggerDelay"])
 
 	status["setTriggerChannelDirections"] = ps.ps6000SetTriggerChannelDirections(
@@ -336,25 +285,25 @@ def main():
 			)
 	assert_pico_ok(status["setTriggerChannelDirections"])
 	
-	""" Get timebase info & number of pre/post trigger samples to be collected
-	noSamples = maxSamples
+	""" Get params["timebase"] info & number of pre/post trigger samples to be collected
+	noSamples = params["maxSamples"]
 	pointer to timeIntervalNanoseconds = byref(timeIntervalns)
 	oversample = 1
-	pointer to maxSamples = byref(returnedMaxSamples)
+	pointer to params["maxSamples"] = byref(returnedMaxSamples)
 	segment index = 0
 	timebase = 1 = 400ps
 	"""
 	timeIntervalns = c_float()
 	returnedMaxSamples = c_int32()
 	status["getTimebase2"] = ps.ps6000GetTimebase2(
-			chandle, timebase, maxSamples, byref(timeIntervalns), 1,
+			chandle, params["timebase"], params["maxSamples"], byref(timeIntervalns), 1,
 			byref(returnedMaxSamples), 0
 			)
 	assert_pico_ok(status["getTimebase2"])
 
-	""" Overflow location and converted type maxSamples. """
+	""" Overflow location and converted type maxSamples """
 	overflow = c_int16()
-	cmaxSamples = c_int32(maxSamples)
+	cmaxSamples = c_int32(params["maxSamples"])
 
 	""" Maximum ADC count value """
 	maxADC = c_int16(32512)
@@ -371,10 +320,10 @@ def main():
 		""" Logging capture """
 		if runtimeOptions["log"]:
 			log(loghandle, f"==> Beginning capture no. {icap + 1}", time=True)
-		
+
 		""" Run block capture
-		number of pre-trigger samples = preTrigSamples
-		number of post-trigger samples = postTrigSamples
+		number of pre-trigger samples = params["preTrigSamples"]
+		number of post-trigger samples = params["postTrigSamples"]
 		timebase = 1 = 400ps
 		oversample = 0
 		time indisposed ms = None
@@ -383,7 +332,8 @@ def main():
 		pParameter = None
 		"""
 		status["runBlock"] = ps.ps6000RunBlock(
-				chandle, preTrigSamples, postTrigSamples, timebase, 0, None, 0, None, None
+				chandle, params["preTrigSamples"], params["postTrigSamples"],
+				params["timebase"], 0, None, 0, None, None
 				)
 		assert_pico_ok(status["runBlock"])
 
@@ -397,25 +347,25 @@ def main():
 		'bufferXMin' are generally used for downsampling but in our case they're equal
 		bufferXMax as we don't need to downsample.
 		"""
-		bufferAMax = (c_int16 * maxSamples)()
-		bufferAMin = (c_int16 * maxSamples)()
-		bufferCMax = (c_int16 * maxSamples)()
-		bufferCMin = (c_int16 * maxSamples)()
+		bufferAMax = (c_int16 * params["maxSamples"])()
+		bufferAMin = (c_int16 * params["maxSamples"])()
+		bufferCMax = (c_int16 * params["maxSamples"])()
+		bufferCMin = (c_int16 * params["maxSamples"])()
 
 		""" Set data buffers location for data collection from channels A and C.
 		sources = ChA, ChC = 0, 2
 		pointer to buffers max = byref(bufferXMax)
 		pointer to buffers min = byref(bufferXMin)
-		buffers length = maxSamples
+		buffers length = params["maxSamples"]
 		ratio mode = PS6000_RATIO_MODE_NONE = 0 
 		"""
 		status["setDataBuffersA"] = ps.ps6000SetDataBuffers(
-				chandle, 0, byref(bufferAMax), byref(bufferAMin), maxSamples, 0
+				chandle, 0, byref(bufferAMax), byref(bufferAMin), params["maxSamples"], 0
 				)
 		assert_pico_ok(status["setDataBuffersA"])
 
 		status["setDataBuffersC"] = ps.ps6000SetDataBuffers(
-				chandle, 2, byref(bufferCMax), byref(bufferCMin), maxSamples, 0
+				chandle, 2, byref(bufferCMax), byref(bufferCMin), params["maxSamples"], 0
 				)
 		assert_pico_ok(status["setDataBuffersC"])		
 
@@ -424,7 +374,7 @@ def main():
 		pointer to number of samples = byref(cmaxSamples)
 		downsample ratio = 1
 		downsample ratio mode = PS6000_RATIO_MODE_NONE
-		pointer to overflow = byref(overflow))
+		pointer to overflow = byref(overflow)
 		"""
 		status["getValues"] = ps.ps6000GetValues(
 				chandle, 0, byref(cmaxSamples), 1, 0, 0, byref(overflow)
@@ -432,27 +382,29 @@ def main():
 		assert_pico_ok(status["getValues"])		
 
 		""" Convert ADC counts data to mV """
-		bufferChAmV = adc2mV(bufferAMax, chRange, maxADC)
-		bufferChCmV = adc2mV(bufferCMax, chRange, maxADC)
+		bufferChAmV = adc2mV(bufferAMax, params["chRange"], maxADC)
+		bufferChCmV = adc2mV(bufferCMax, params["chRange"], maxADC)
 
 		""" Removing the analog offset from data points """
-		thresholdmV = (thresholdADC * chInputRanges[chRange]) / maxADC.value - (analogOffset * 1000)
-		for i in range(maxSamples):
-			bufferChAmV[i] -= (analogOffset * 1000)
-			bufferChCmV[i] -= (analogOffset * 1000)
+		thresholdmV = (params["thresholdADC"] * chInputRanges[params["chRange"]]) \
+				/ maxADC.value - (params["analogOffset"] * 1000)
+		for i in range(params["maxSamples"]):
+			bufferChAmV[i] -= (params["analogOffset"] * 1000)
+			bufferChCmV[i] -= (params["analogOffset"] * 1000)
 
 		""" Create time data """
 		time = np.linspace(
-				0, (cmaxSamples.value - 1) * timeIntervalns.value, cmaxSamples.value, dtype="float32"
+				0, (cmaxSamples.value - 1) * timeIntervalns.value,
+				cmaxSamples.value, dtype="float32"
 				)
 
 		""" Detect datapoints where the threshold was hit (both falling & rising edge) """
 		gate = {"chA": {}, "chC": {}}
 		gate["chA"] = detect_gate_open_closed(
-				bufferChAmV, time, thresholdmV, maxSamples, timeIntervalns.value
+				bufferChAmV, time, thresholdmV, params["maxSamples"], timeIntervalns.value
 				)
 		gate["chC"] = detect_gate_open_closed(
-				bufferChCmV, time, thresholdmV, maxSamples, timeIntervalns.value
+				bufferChCmV, time, thresholdmV, params["maxSamples"], timeIntervalns.value
 				)
 		""" Logging threshold hits """
 		if runtimeOptions["log"]:
