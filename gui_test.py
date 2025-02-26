9c6d638083b5cb2aef7a4fd44ffed0627a24dfcd
9iovaferra <ferrari.g1308@gmail.com>
2025-02-26 12:32:45 +0100

    Populated the Settings tab

--- gui_test.py
+++ gui_test.py
@@ -1,6 +1,7 @@
 """ tkSliderWidget Copyright (c) 2020, Mengxun Li """
 from tkinter import *
 from tkinter import ttk
+from pycoviewlib.constants import chInputRanges
 from matplotlib.figure import Figure 
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from pycoviewlib.tkSliderWidget.tkSliderWidget import Slider
@@ -29,27 +30,28 @@ params["maxSamples"] = params["preTrigSamples"] + params["postTrigSamples"]
 
 padding = {"padx": 5, "pady": 5, "ipadx": 5, "ipady": 5}
 
+""" Main window & frame """
 root = Tk()
 root.title("PycoView")
-# root.geometry("600x400")
 
-topframe = ttk.Frame(root, padding="3 3 12 12")
+# padding is: bottom, top, right, left
+topframe = ttk.Frame(root, padding="5 5 5 5")
 topframe.grid(column=0, row=0, **padding, sticky=N+W+E)
 topframe.columnconfigure(0, weight=1)
+topframe.columnconfigure(1, weight=1)
 topframe.columnconfigure(2, weight=3)
 topframe.rowconfigure(0, weight=1)
 
-ttk.Label(topframe, text="v1.00", anchor=E).grid(column=2, row=0, sticky=W+E)
-
-""" Mode selector """
 modes = ["ADC", "TDC", "Meantimer"]
 ttk.Label(topframe, text="Mode:").grid(column=0, row=0, sticky=W)
 modeSelector = ttk.Combobox(topframe, state="readonly", values=modes)
 modeSelector.current(0)
 modeSelector.grid(column=1, row=0, sticky=N+W)
 
+ttk.Label(topframe, text="v1.00", anchor=E).grid(column=2, row=0, sticky=W+E)
+
 """ Tabs """
-tabsframe = ttk.Frame(root, padding="3 3 12 12")
+tabsframe = ttk.Frame(root, padding="5 0 5 5")
 tabsframe.grid(column=0, row=1, **padding, sticky=W+E+S)
 tabControl = ttk.Notebook(tabsframe)
 runTab = ttk.Frame(tabControl)
@@ -70,8 +72,8 @@ startButton.grid(column=0, row=1, **padding, sticky=E+S)
 stopButton = ttk.Button(runTab, text="STOP", command=placeholder)
 stopButton.grid(column=1, row=1, **padding, sticky=W+S)
 
-histogram = ttk.Labelframe(runTab, text="Histogram")
-histogram.grid(column=2, row=0, columnspan=5, rowspan=2, **padding, sticky=W+E+N+S)
+histogram = ttk.Labelframe(runTab, text="Histogram", height=500)
+histogram.grid(column=2, row=0, columnspan=5, rowspan=2, **padding,  sticky=W+E+N+S)
 make_histogram()
 rangeSlider = Slider(runTab,
 				width=200,
@@ -84,7 +86,7 @@ rangeSlider = Slider(runTab,
 				removable=False,
 				addable=False
 				)
-rangeSlider.grid(column=2, row=2, rowspan=2, **padding, sticky=W+E+N+S)
+rangeSlider.grid(column=2, row=2, rowspan=2, sticky=W+E+N+S)
 ttk.Label(runTab, text="Bins:", anchor=CENTER).grid(column=3, row=2, sticky=N)
 spinbox_default = DoubleVar(value=100)
 spinbox = Spinbox(runTab, from_=50, to=200, textvariable=spinbox_default, width=7, increment=10)
@@ -96,9 +98,208 @@ resetButton.grid(column=5, row=2, rowspan=2, sticky=E+N+S)
 saveButton = Button(runTab, text="Save", width=3, height=1, command=placeholder)
 saveButton.grid(column=6, row=2, sticky=E+N+S)
 
-# for child in topframe.winfo_children():
-# 	child.grid_configure(padx=5, pady=5)
-# for child in tabsframe.winfo_children():
-# 	child.grid_configure(padx=5, pady=5)
+""" Settings tab """
+acquisition = ttk.Labelframe(settingsTab, text="Acquisition")
+acquisition.grid(column=0, row=0, **padding, sticky=W+E+N)
+
+ttk.Label(acquisition, text="Pre-trigger samples").grid(column=0, row=0, padx=5, pady=5, sticky=W+N) 
+preTrigSpbx = Spinbox(
+		acquisition,
+		from_=0,
+		to=500,
+		textvariable=DoubleVar(value=params["preTrigSamples"]),
+		width=7,
+		increment=1
+		)
+preTrigSpbx.grid(column=0, row=1, padx=5, sticky=W+N)
+
+ttk.Label(acquisition, text="Post-trigger samples").grid(column=0, row=2, padx=5, pady=5, sticky=W+N) 
+postTrigSpbx = Spinbox(
+		acquisition,
+		from_=1,
+		to=500,
+		textvariable=DoubleVar(value=params["postTrigSamples"]),
+		width=7,
+		increment=1
+		)
+postTrigSpbx.grid(column=0, row=3, padx=5, sticky=W+N)
+
+ttk.Label(acquisition, text="Timebase").grid(column=0, row=4, padx=5, pady=5, sticky=W+N) 
+timebases = ["200 ps", "400 ps", "800 ps", "1.6 ns", "3.2 ns"]
+timebaseCbx = ttk.Combobox(acquisition, state="readonly", values=timebases, width=7)
+timebaseCbx.current(0)
+timebaseCbx.grid(column=0, row=5, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(acquisition, text="Threshold (mV)").grid(column=0, row=6, padx=5, pady=5, sticky=W+N) 
+thresholdSpbx = Spinbox(
+		acquisition,
+		from_=-1000, # bounds should update with the channel range
+		to=0,
+		textvariable=DoubleVar(value=-400), # convert from thresholdADC
+		width=7,
+		increment=1
+		)
+thresholdSpbx.grid(column=0, row=7, padx=5, sticky=W+N)
+
+ttk.Label(acquisition, text="Auto trigger (ms)").grid(column=0, row=8, padx=5, pady=5, sticky=W+N) 
+autoTrigSpbx = Spinbox(
+		acquisition,
+		from_=500,
+		to=10000,
+		textvariable=DoubleVar(value=params["autoTrigms"]),
+		width=7,
+		increment=100
+		)
+autoTrigSpbx.grid(column=0, row=9, padx=5, sticky=W+N)
+
+ttk.Label(acquisition, text="Trigger delay (s)").grid(column=0, row=10, padx=5, pady=5, sticky=W+N) 
+delaySpbx = Spinbox(
+		acquisition,
+		from_=0,
+		to=10,
+		textvariable=DoubleVar(value=params["autoTrigms"]),
+		width=7,
+		increment=1
+		)
+delaySpbx.grid(column=0, row=11, padx=5, sticky=W+N)
+
+""" Channel A settings """
+chALbf = ttk.Labelframe(settingsTab, text="Channel A")
+chALbf.grid(column=1, row=0, **padding, sticky=W+E+N)
+
+chAEnabledFlag = IntVar()
+chAEnabled = Checkbutton(chALbf, variable=chAEnabledFlag, text="Enabled", onvalue=1, offvalue=0, command=placeholder)
+chAEnabled.grid(column=0, row=0, pady=5, sticky=W+N)
+
+ttk.Label(chALbf, text="Range (mV)").grid(column=0, row=1, padx=5, pady=5, sticky=W+N) 
+chRangeCbx = ttk.Combobox(chALbf, state="readonly", values=chInputRanges, width=7)
+chRangeCbx.current(0)
+chRangeCbx.grid(column=0, row=2, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chALbf, text="Coupling").grid(column=0, row=3, padx=5, pady=5, sticky=W+N) 
+coupling = ["AC 1MΩ", "DC 1MΩ", "DC 50Ω"]
+couplingCbx = ttk.Combobox(chALbf, state="readonly", values=coupling, width=7)
+couplingCbx.current(0)
+couplingCbx.grid(column=0, row=4, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chALbf, text="Analogue offset (mV)").grid(column=0, row=5, padx=5, pady=5, sticky=W+N) 
+analogOffsetSpbx = Spinbox(
+		chALbf,
+		from_=0,
+		to=500, # should update with channel range too
+		textvariable=DoubleVar(value=params["analogOffset"]),
+		width=7,
+		increment=1
+		)
+analogOffsetSpbx.grid(column=0, row=6, padx=5, sticky=W+N)
+
+ttk.Label(chALbf, text="Bandwidth").grid(column=0, row=7, padx=5, pady=5, sticky=W+N) 
+bandwidth = ["Full", "25MHz"]
+bandwidthCbx = ttk.Combobox(chALbf, state="readonly", values=bandwidth, width=7)
+bandwidthCbx.current(0)
+bandwidthCbx.grid(column=0, row=8, padx=5, pady=5, sticky=W+N)
+
+""" Channel B settings """
+chBLbf = ttk.Labelframe(settingsTab, text="Channel B")
+chBLbf.grid(column=2, row=0, **padding, sticky=W+E+N)
+
+chAEnabledFlag = IntVar()
+chBEnabled = Checkbutton(chBLbf, variable=chAEnabledFlag, text="Enabled", onvalue=1, offvalue=0, command=placeholder)
+chBEnabled.grid(column=0, row=0, pady=5, sticky=W+N)
+
+ttk.Label(chBLbf, text="Range (mV)").grid(column=0, row=1, padx=5, pady=5, sticky=W+N) 
+chRangeCbx = ttk.Combobox(chBLbf, state="readonly", values=chInputRanges, width=7)
+chRangeCbx.current(0)
+chRangeCbx.grid(column=0, row=2, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chBLbf, text="Coupling").grid(column=0, row=3, padx=5, pady=5, sticky=W+N) 
+couplingCbx = ttk.Combobox(chBLbf, state="readonly", values=coupling, width=7)
+couplingCbx.current(0)
+couplingCbx.grid(column=0, row=4, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chBLbf, text="Analogue offset (mV)").grid(column=0, row=5, padx=5, pady=5, sticky=W+N) 
+analogOffsetSpbx = Spinbox(
+		chBLbf,
+		from_=0,
+		to=500, # should update with channel range too
+		textvariable=DoubleVar(value=params["analogOffset"]),
+		width=7,
+		increment=1
+		)
+analogOffsetSpbx.grid(column=0, row=6, padx=5, sticky=W+N)
+
+ttk.Label(chBLbf, text="Bandwidth").grid(column=0, row=7, padx=5, pady=5, sticky=W+N) 
+bandwidthCbx = ttk.Combobox(chBLbf, state="readonly", values=bandwidth, width=7)
+bandwidthCbx.current(0)
+bandwidthCbx.grid(column=0, row=8, padx=5, pady=5, sticky=W+N)
+
+""" Channel C settings """
+chCLbf = ttk.Labelframe(settingsTab, text="Channel C")
+chCLbf.grid(column=3, row=0, **padding, sticky=W+E+N)
+
+chCEnabledFlag = IntVar()
+chCEnabled = Checkbutton(chCLbf, variable=chCEnabledFlag, text="Enabled", onvalue=1, offvalue=0, command=placeholder)
+chCEnabled.grid(column=0, row=0, pady=5, sticky=W+N)
+
+ttk.Label(chCLbf, text="Range (mV)").grid(column=0, row=1, padx=5, pady=5, sticky=W+N) 
+chRangeCbx = ttk.Combobox(chCLbf, state="readonly", values=chInputRanges, width=7)
+chRangeCbx.current(0)
+chRangeCbx.grid(column=0, row=2, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chCLbf, text="Coupling").grid(column=0, row=3, padx=5, pady=5, sticky=W+N) 
+couplingCbx = ttk.Combobox(chCLbf, state="readonly", values=coupling, width=7)
+couplingCbx.current(0)
+couplingCbx.grid(column=0, row=4, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chCLbf, text="Analogue offset (mV)").grid(column=0, row=5, padx=5, pady=5, sticky=W+N) 
+analogOffsetSpbx = Spinbox(
+		chCLbf,
+		from_=0,
+		to=500, # should update with channel range too
+		textvariable=DoubleVar(value=params["analogOffset"]),
+		width=7,
+		increment=1
+		)
+analogOffsetSpbx.grid(column=0, row=6, padx=5, sticky=W+N)
+
+ttk.Label(chCLbf, text="Bandwidth").grid(column=0, row=7, padx=5, pady=5, sticky=W+N) 
+bandwidthCbx = ttk.Combobox(chCLbf, state="readonly", values=bandwidth, width=7)
+bandwidthCbx.current(0)
+bandwidthCbx.grid(column=0, row=8, padx=5, pady=5, sticky=W+N)
+
+""" Channel D settings """
+chDLbf = ttk.Labelframe(settingsTab, text="Channel C")
+chDLbf.grid(column=4, row=0, **padding, sticky=W+E+N)
+
+chDEnabledFlag = IntVar()
+chDEnabled = Checkbutton(chDLbf, variable=chDEnabledFlag, text="Enabled", onvalue=1, offvalue=0, command=placeholder)
+chDEnabled.grid(column=0, row=0, pady=5, sticky=W+N)
+
+ttk.Label(chDLbf, text="Range (mV)").grid(column=0, row=1, padx=5, pady=5, sticky=W+N) 
+chRangeCbx = ttk.Combobox(chDLbf, state="readonly", values=chInputRanges, width=7)
+chRangeCbx.current(0)
+chRangeCbx.grid(column=0, row=2, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chDLbf, text="Coupling").grid(column=0, row=3, padx=5, pady=5, sticky=W+N) 
+couplingCbx = ttk.Combobox(chDLbf, state="readonly", values=coupling, width=7)
+couplingCbx.current(0)
+couplingCbx.grid(column=0, row=4, padx=5, pady=5, sticky=W+N)
+
+ttk.Label(chDLbf, text="Analogue offset (mV)").grid(column=0, row=5, padx=5, pady=5, sticky=W+N) 
+analogOffsetSpbx = Spinbox(
+		chDLbf,
+		from_=0,
+		to=500, # should update with channel range too
+		textvariable=DoubleVar(value=params["analogOffset"]),
+		width=7,
+		increment=1
+		)
+analogOffsetSpbx.grid(column=0, row=6, padx=5, sticky=W+N)
+
+ttk.Label(chDLbf, text="Bandwidth").grid(column=0, row=7, padx=5, pady=5, sticky=W+N) 
+bandwidthCbx = ttk.Combobox(chDLbf, state="readonly", values=bandwidth, width=7)
+bandwidthCbx.current(0)
+bandwidthCbx.grid(column=0, row=8, padx=5, pady=5, sticky=W+N)
+
 
 root.mainloop()
