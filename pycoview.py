87c39c1044929a10cdafd7670635f02a1671f118
9iovaferra <ferrari.g1308@gmail.com>
2025-03-06 10:49:00 +0100

    Removed pycoview.py (gui_test will become new main script)

--- pycoview.py
+++
@@ -1,9 +0,0 @@
-""" Copyright (C) 2019 Pico Technology Ltd. """
-import subprocess
-from os import path
-
-python = path.expanduser("~/.venv/bin/python3")
-exitCode = subprocess.call([python, "tdc.py", "150", "log"])
-
-if exitCode != 0:
-	print("/!\\")
