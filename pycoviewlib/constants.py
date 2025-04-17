0e922a8b23dc158053f375c30ecc584824899d71
9iovaferra <ferrari.g1308@gmail.com>
2025-04-17 14:51:57 +0200

    Better couplings dict format. Added PS_6000_CONDITION_TRUE/DONT_CARE, PS_6000_BELOW/NONE

--- pycoviewlib/constants.py
+++ pycoviewlib/constants.py
@@ -1,3 +1,5 @@
+from picosdk.ps6000 import ps6000 as ps
+
 chInputRanges = [
 	10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000,
 	50000, 100000, 200000
@@ -8,5 +10,10 @@ maxADC = 32512
 channelIDs = ["A", "B", "C", "D"]
 modes = {"ADC": "adc", "TDC": "tdc", "Meantimer": "mntm"}
 timebases = ["200 ps", "400 ps", "800 ps", "1.6 ns", "3.2 ns"]
-couplings = {"DC 50Ω": 50, "DC 1MΩ": 1000000, "AC 1MΩ": 1000000}
+couplings = {"AC 1MΩ": (0,1000000), "DC 1MΩ": (1,1000000), "DC 50Ω": (2,50)}
 bandwidths = {"Full": 0, "25MHz": 1}
+
+PS6000_CONDITION_TRUE = ps.PS6000_TRIGGER_STATE["PS6000_CONDITION_TRUE"]
+PS6000_CONDITION_DONT_CARE = ps.PS6000_TRIGGER_STATE["PS6000_CONDITION_DONT_CARE"]
+PS6000_BELOW = ps.PS6000_THRESHOLD_DIRECTION["PS6000_BELOW"]
+PS6000_NONE = ps.PS6000_THRESHOLD_DIRECTION["PS6000_NONE"]
